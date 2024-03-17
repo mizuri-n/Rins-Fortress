863934dfb57603f3775a3bb55676efd83b8f5b6f
Rin <cxr6549@mavs.uta.edu>
2024-03-17 18:29:11 -0500

    almost finished

--- src/main/java/turniplabs/shieldmod/ShieldMod.java
+++ src/main/java/turniplabs/shieldmod/ShieldMod.java
@@ -20,13 +20,13 @@ public class ShieldMod implements ModInitializer, RecipeEntrypoint {
     public static final String MOD_ID = "shield_mod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final Item woodenShield = ItemHelper.createItem(MOD_ID, new ShieldItem("wooden.shield", 17000, ShieldMaterials.TOOL_WOOD),"wooden_shield.png");
-	public static final Item stoneShield = ItemHelper.createItem(MOD_ID, new ShieldItem("stone.shield", 17001, ShieldMaterials.TOOL_STONE), "stone_shield.png");
-	public static final Item ironShield = ItemHelper.createItem(MOD_ID, new ShieldItem("iron.shield", 17002, ShieldMaterials.TOOL_IRON), "iron_shield.png");
-	public static final Item goldShield = ItemHelper.createItem(MOD_ID, new ShieldItem("gold.shield", 17003, ShieldMaterials.TOOL_GOLD), "gold_shield.png");
-	public static final Item diamondShield = ItemHelper.createItem(MOD_ID, new ShieldItem("diamond.shield", 17004, ShieldMaterials.TOOL_DIAMOND), "diamond_shield.png");
+	public static final Item woodenShield = ItemHelper.createItem(MOD_ID, new ShieldItem("wooden.shield", 17000, ShieldMaterials.TOOL_WOOD),"wooden_shield.png").withTags(ItemTags.preventCreativeMining);
+	public static final Item stoneShield = ItemHelper.createItem(MOD_ID, new ShieldItem("stone.shield", 17001, ShieldMaterials.TOOL_STONE), "stone_shield.png").withTags(ItemTags.preventCreativeMining);
+	public static final Item ironShield = ItemHelper.createItem(MOD_ID, new ShieldItem("iron.shield", 17002, ShieldMaterials.TOOL_IRON), "iron_shield.png").withTags(ItemTags.preventCreativeMining);
+	public static final Item goldShield = ItemHelper.createItem(MOD_ID, new ShieldItem("gold.shield", 17003, ShieldMaterials.TOOL_GOLD), "gold_shield.png").withTags(ItemTags.preventCreativeMining);
+	public static final Item diamondShield = ItemHelper.createItem(MOD_ID, new ShieldItem("diamond.shield", 17004, ShieldMaterials.TOOL_DIAMOND), "diamond_shield.png").withTags(ItemTags.preventCreativeMining);
 
-	public static final Item steelShield = ItemHelper.createItem(MOD_ID, new ShieldItem("steel.shield", 17005, ShieldMaterials.TOOL_STEEL), "steel_shield.png");
+	public static final Item steelShield = ItemHelper.createItem(MOD_ID, new ShieldItem("steel.shield", 17005, ShieldMaterials.TOOL_STEEL), "steel_shield.png").withTags(ItemTags.preventCreativeMining);
 
 
 
@@ -37,39 +37,39 @@ public class ShieldMod implements ModInitializer, RecipeEntrypoint {
 	@Override
 	public void onRecipesReady() {
 		RecipeBuilder.Shaped(MOD_ID)
-			.setShape("PLP","PPP"," P ")
-			.addInput('P', Block.planksOak)
-			.addInput('L',Block.logOak)
+			.setShape(" P ","PLP"," P ")
+			.addInput('P', "minecraft:planks")
+			.addInput('L',"minecraft:logs")
 			.create("woodenShield", woodenShield.getDefaultStack());
 
 		RecipeBuilder.Shaped(MOD_ID)
-			.setShape("PLP","PPP"," P ")
-			.addInput('P', Block.planksOak)
-			.addInput('L',Block.stone)
+			.setShape(" P ","PLP"," P ")
+			.addInput('P', Block.cobbleStone)
+			.addInput('L',"minecraft:planks")
 			.create("stoneShield", stoneShield.getDefaultStack());
 
 		RecipeBuilder.Shaped(MOD_ID)
-			.setShape("PLP","PPP"," P ")
-			.addInput('P', Block.planksOak)
-			.addInput('L',Block.blockIron)
+			.setShape(" P ","PLP"," P ")
+			.addInput('P', Item.ingotIron)
+			.addInput('L',"minecraft:planks")
 			.create("ironShield", ironShield.getDefaultStack());
 
 		RecipeBuilder.Shaped(MOD_ID)
-			.setShape("PLP","PPP"," P ")
-			.addInput('P', Block.planksOak)
-			.addInput('L',Block.blockGold)
+			.setShape(" P ","PLP"," P ")
+			.addInput('P', Item.ingotGold)
+			.addInput('L',"minecraft:planks")
 			.create("goldShield", goldShield.getDefaultStack());
 
 		RecipeBuilder.Shaped(MOD_ID)
-			.setShape("PLP","PPP"," P ")
-			.addInput('P', Block.planksOak)
-			.addInput('L',Block.blockDiamond)
+			.setShape(" P ","PLP"," P ")
+			.addInput('P', Item.diamond)
+			.addInput('L',"minecraft:planks")
 			.create("diamondShield", diamondShield.getDefaultStack());
 
 		RecipeBuilder.Shaped(MOD_ID)
-			.setShape("PLP","PPP"," P ")
-			.addInput('P', Block.planksOak)
-			.addInput('L',Block.blockSteel)
+			.setShape(" P ","PLP"," P ")
+			.addInput('P', Item.ingotSteel)
+			.addInput('L',"minecraft:planks")
 			.create("woodenShield", steelShield.getDefaultStack());
 
 
