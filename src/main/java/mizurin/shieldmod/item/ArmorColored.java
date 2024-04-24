a1aac7e3d8059b56a3f375608d5b2d98767fd43d
Rin <cxr6549@mavs.uta.edu>
2024-04-24 00:58:53 -0500

    update to 7.1

---
+++ src/main/java/mizurin/shieldmod/item/ArmorColored.java
@@ -0,0 +1,46 @@
+package mizurin.shieldmod.item;
+
+import com.mojang.nbt.CompoundTag;
+import mizurin.shieldmod.ShieldMod;
+import net.minecraft.core.item.ItemArmor;
+import net.minecraft.core.item.material.ArmorMaterial;
+import turniplabs.halplibe.helper.TextureHelper;
+import net.minecraft.core.item.ItemStack;
+import useless.prismaticlibe.ColoredArmorTexture;
+import useless.prismaticlibe.ColoredTexture;
+import useless.prismaticlibe.IColored;
+import useless.prismaticlibe.IColoredArmor;
+
+import java.awt.*;
+
+public class ArmorColored extends ItemArmor implements IColoredArmor, IColored {
+	public static final String MOD_ID = ShieldMod.MOD_ID;
+	public static final int[][] baseColor = new int[][] {
+		TextureHelper.getOrCreateItemTexture(ShieldMod.MOD_ID, "leather_helmet.png"),
+		TextureHelper.getOrCreateItemTexture(ShieldMod.MOD_ID, "leather_chestplate.png"),
+		TextureHelper.getOrCreateItemTexture(ShieldMod.MOD_ID, "leather_leggings.png"),
+		TextureHelper.getOrCreateItemTexture(ShieldMod.MOD_ID, "leather_boots.png")};
+
+	public ArmorColored(String name, int id, ArmorMaterial material, int armorPiece) {
+		super(name, id, material, armorPiece);
+	}
+	public Color getColor(ItemStack itemStack){
+		if (itemStack.getData().containsKey("dyed_color")){
+			CompoundTag colorTag = itemStack.getData().getCompound("dyed_color");
+			int red = colorTag.getShort("red");
+			int green = colorTag.getShort("green");
+			int blue = colorTag.getShort("blue");
+			return new Color(red, green, blue);
+		}
+		return new Color(255, 255,255);
+	}
+	@Override
+	public ColoredTexture[] getTextures(ItemStack itemStack) {
+		return new ColoredTexture[]{new ColoredTexture(baseColor[armorPiece], getColor(itemStack))};
+	}
+
+	@Override
+	public ColoredArmorTexture[] getArmorTextures(ItemStack itemStack) {
+		return new ColoredArmorTexture[]{new ColoredArmorTexture("leather", getColor(itemStack))};
+	}
+}
