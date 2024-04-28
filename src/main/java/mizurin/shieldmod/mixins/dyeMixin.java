ab3e6c2c4f2a1cf4d4cd2d046181e3d7a8ca7acb
Rin <cxr6549@mavs.uta.edu>
2024-04-27 23:05:44 -0500

    test in progress

--- src/main/java/mizurin/shieldmod/mixins/dyeMixin.java
+++ src/main/java/mizurin/shieldmod/mixins/dyeMixin.java
@@ -3,10 +3,7 @@ package mizurin.shieldmod.mixins;
 import mizurin.shieldmod.ShieldAchievements;
 import mizurin.shieldmod.item.Shields;
 import net.minecraft.core.entity.player.EntityPlayer;
-import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.player.inventory.IInventory;
-import net.minecraft.core.player.inventory.slot.Slot;
 import net.minecraft.core.player.inventory.slot.SlotCrafting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,20 +12,34 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = SlotCrafting.class, remap = false)
-public abstract class dyeMixin extends Slot {
+public class dyeMixin {
+
 	@Shadow
 	private EntityPlayer thePlayer;
 
-	public dyeMixin(IInventory inventory, int id, int x, int y) {
-		super(inventory, id, x, y);
-	}
-	@Inject(method = "onPickupFromSlot", at = @At("HEAD"), cancellable = false)
+	@Inject(method = "onPickupFromSlot(Lnet/minecraft/core/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/item/ItemStack;onCrafting(Lnet/minecraft/core/world/World;Lnet/minecraft/core/entity/player/EntityPlayer;)V", shift = At.Shift.AFTER))
 	public void injectPickup(ItemStack itemStack, CallbackInfo ci){
-		Item item = itemStack.getItem();
-		if(item.id == Shields.armorLeatherHelmet.id && itemStack.getData().containsKey("dyed_color")) thePlayer.addStat(ShieldAchievements.COLORS, 1);
-		if(item.id == Shields.armorLeatherChest.id && itemStack.getData().containsKey("dyed_color")) thePlayer.addStat(ShieldAchievements.COLORS, 1);
-		if(item.id == Shields.armorLeatherLeg.id && itemStack.getData().containsKey("dyed_color")) thePlayer.addStat(ShieldAchievements.COLORS, 1);
-		if(item.id == Shields.armorLeatherBoot.id && itemStack.getData().containsKey("dyed_color")) thePlayer.addStat(ShieldAchievements.COLORS, 1);
-		if(item.id == Shields.leatherShield.id && itemStack.getData().containsKey("dyed_color")) thePlayer.addStat(ShieldAchievements.COLORS, 1);
+		if(itemStack.itemID == Shields.woodenShield.id){
+			thePlayer.addStat(ShieldAchievements.SHIELD_GOT, 1);
+		}
+		if(itemStack.itemID == Shields.steelShield.id){
+			thePlayer.addStat(ShieldAchievements.MODERN_AGE, 1);
+		}
+		if(itemStack.itemID == Shields.armorLeatherHelmet.id && itemStack.getData().containsKey("dyed_color")){
+			thePlayer.addStat(ShieldAchievements.COLORS, 1);
+		}
+		if(itemStack.itemID == Shields.armorLeatherChest.id && itemStack.getData().containsKey("dyed_color")){
+			thePlayer.addStat(ShieldAchievements.COLORS, 1);
+		}
+		if(itemStack.itemID == Shields.armorLeatherLeg.id && itemStack.getData().containsKey("dyed_color")){
+			thePlayer.addStat(ShieldAchievements.COLORS, 1);
+		}
+		if(itemStack.itemID == Shields.armorLeatherBoot.id && itemStack.getData().containsKey("dyed_color")){
+			thePlayer.addStat(ShieldAchievements.COLORS, 1);
+		}
+		if(itemStack.itemID == Shields.leatherShield.id && itemStack.getData().containsKey("dyed_color")){
+			thePlayer.addStat(ShieldAchievements.COLORS, 1);
+		}
+
 	}
 }
