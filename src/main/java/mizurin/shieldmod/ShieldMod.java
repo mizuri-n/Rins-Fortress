1906c97ca93ebf5693a9e942156172f30f8d57b5
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-06-10 15:54:28 -0500

    Fixed server crashing on startup

--- src/main/java/mizurin/shieldmod/ShieldMod.java
+++ src/main/java/mizurin/shieldmod/ShieldMod.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.helper.AchievementHelper;
 import turniplabs.halplibe.helper.EntityHelper;
+import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.ConfigHandler;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import mizurin.shieldmod.item.Shields;
@@ -18,7 +19,7 @@ import turniplabs.halplibe.util.achievements.AchievementPage;
 
 import java.util.Properties;
 
-public class ShieldMod implements ModInitializer, GameStartEntrypoint {
+public class ShieldMod implements ModInitializer, GameStartEntrypoint, ClientStartEntrypoint {
     public static final String MOD_ID = "shieldmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -44,8 +45,6 @@ public class ShieldMod implements ModInitializer, GameStartEntrypoint {
 		//AchievementPage SHIELDACHIEVEMENTS;
 		//SHIELDACHIEVEMENTS = new ShieldAchievements();
 		AchievementHelper.addPage(new ShieldAchievements());
-		EntityHelper.Client.assignEntityRenderer(EntityShield.class, new SnowballRenderer(Shields.ammotearShield.getIconFromDamage(0)));
-
 	}
 
 	@Override
@@ -53,5 +52,15 @@ public class ShieldMod implements ModInitializer, GameStartEntrypoint {
 		LookupFuelFurnace.instance.addFuelEntry(Shields.woodenShield.id, 600);
 		new recipes().initializeRecipe();
 		LOGGER.info("Time to Block!");
+	}
+
+	@Override
+	public void beforeClientStart() {
+		EntityHelper.Client.assignEntityRenderer(EntityShield.class, new SnowballRenderer(Shields.ammotearShield.getIconFromDamage(0)));
+	}
+
+	@Override
+	public void afterClientStart() {
+
 	}
 }
