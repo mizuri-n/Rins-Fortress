f688995ae03223ef3be982a07ff2dd532994add0
Rin <rin@rin-machine.attlocal.net>
2024-11-01 18:33:35 -0500

    pain

--- src/main/java/mizurin/shieldmod/mixins/DazedMixin.java
+++ src/main/java/mizurin/shieldmod/mixins/DazedMixin.java
@@ -2,7 +2,9 @@ package mizurin.shieldmod.mixins;
 
 import mizurin.shieldmod.interfaces.IDazed;
 import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.EntityLiving;
 import net.minecraft.core.util.helper.DamageType;
+import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -10,75 +12,78 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Entity.class, remap = false)
-public class DazedMixin implements IDazed {
+
+@Mixin(value = EntityLiving.class, remap = false)
+public abstract class DazedMixin extends Entity implements IDazed {
+
+
+	@Unique
+	private static final int DATA_DAZE = 24;
+
+	@Unique
+	private static final int DATA_FREEZE = 25;
+
+	public DazedMixin(World world) {
+		super(world);
+	}
+
 
 	@Shadow
 	public boolean hurt(Entity attacker, int damage, DamageType type) {
 		return false;
 	}
 
-	@Shadow
-	public double xd;
-
-	@Shadow
-	public double zd;
-
-
-	@Unique
-	public int remainingDazedTicks;
-
-	@Unique
-	public int remainingFreezeTicks;
+	@Inject(method = "init()V", at = @At("TAIL"))
+	public void defineSyncStatus(CallbackInfo ci){
+		entityData.define(DATA_DAZE, 0);
+		entityData.define(DATA_FREEZE, 0);
+	}
 
 
 	//Mirrors the fire status effect but cannot be put out and slows the entity.
 	@Inject(method = "baseTick()V", at = @At("HEAD"))
 	public void inject(CallbackInfo callbackInfo) {
-		if (this.remainingDazedTicks > 0) {
+		if (this.shieldmod$getDazedHurt() > 0) {
 
 			this.xd *= 0.85D;
 			this.zd *= 0.85D;
 
-			if (this.remainingDazedTicks % 16 == 0) {
+			if (this.shieldmod$getDazedHurt() % 16 == 0) {
 				this.hurt( null, 1, DamageType.COMBAT);
 			}
 
-			--this.remainingDazedTicks;
-			--this.remainingDazedTicks;
-			--this.remainingDazedTicks;
+			this.entityData.set(DATA_DAZE, this.entityData.getInt(DATA_DAZE) - 3);
 			//the ticks are put in a way to deal small damage and slow while also being offset by fire status
 			//having it mimic the fire status countdown causes them to overlap and only deal damage once due to invulnerability frames.
 		}
-		if (this.remainingFreezeTicks > 0) {
+		if (this.shieldmod$getFreezeHurt() > 0) {
 			this.xd *= 0.65D;
 			this.zd *= 0.65D;
 
-			--this.remainingFreezeTicks;
+			this.entityData.set(DATA_FREEZE, this.entityData.getInt(DATA_FREEZE) - 1);
 
 		}
 
 	}
 	@Override
 	public void shieldmod$dazedHurt(int dazedTicks){
-		this.hurt(null, 1, DamageType.COMBAT);
-		this.remainingDazedTicks = dazedTicks;
+		this.entityData.set(DATA_DAZE, dazedTicks);
 	}
 
 	@Override
 	public int shieldmod$getDazedHurt() {
-		return remainingDazedTicks;
+		return entityData.getInt(DATA_DAZE);
 	}
 
 
 	@Override
 	public void shieldmod$freezeHurt(int freezeTicks) {
-	this.remainingFreezeTicks = freezeTicks;
+		this.entityData.set(DATA_FREEZE, freezeTicks);
 	}
 
 	@Override
 	public int shieldmod$getFreezeHurt() {
-		return remainingFreezeTicks;
+		return entityData.getInt(DATA_FREEZE);
 	}
 
 }
