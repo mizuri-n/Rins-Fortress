247d63dae1a6d2ea5e3d38b00c25413b4fbb164e
Bigjango13 <bigjango13@gmail.com>
2024-11-29 22:58:05 -0800

    Fix potions crashing

--- src/main/java/mizurin/shieldmod/entities/EntityPB.java
+++ src/main/java/mizurin/shieldmod/entities/EntityPB.java
@@ -33,7 +33,7 @@ public class EntityPB extends EntityProjectile {
 
 	@Override
 	public void onHit(HitResult hitResult) {
-		if (hitResult.entity != null) {
+		if (hitResult.entity instanceof EntityLiving) {
 			hitResult.entity.hurt(this.owner, this.damage, DamageType.COMBAT);
 			((IDazed) hitResult.entity).shieldmod$dazedHurt(300);
 			//Applies my custom status effect from the IDazed interface.
